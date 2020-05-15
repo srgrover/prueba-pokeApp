d67369e70a6006d6f07884dbbf12182bd9b887ff
srgrover <djgrovezone@gmail.com>
2020-05-15 20:57:32 +0200

    Creamos y completamos el formulario

---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,44 @@
+import { PokeApiService } from './../services/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.css']
+})
+export class FormularioComponent implements OnInit {
+  public pokeForm: FormGroup;
+  public myPokemons;
+  public types;
+  
+  constructor(private pokeApi: PokeApiService) {
+    this.pokeForm = new FormGroup({
+      'name': new FormControl(),
+      'weight': new FormControl(),
+      'type': new FormControl(),
+      'image': new FormControl()
+    });
+  }
+
+  ngOnInit(): void {
+    let pokemonsAlmacenados = JSON.parse(localStorage.getItem("myPokemons"));
+    this.myPokemons = pokemonsAlmacenados ? pokemonsAlmacenados : [];
+    this.getTypes();
+  }
+
+  guardarCambios() {
+    this.myPokemons.push(this.pokeForm.value);
+    localStorage.setItem("myPokemons", JSON.stringify(this.myPokemons));
+    this.resetForm(this.pokeForm);
+  }
+
+  resetForm(pokeForm){
+    pokeForm.reset();
+  }
+
+  async getTypes(){
+    const response: any = await this.pokeApi.getTypes();
+    this.types = response.results;
+  }
+}
